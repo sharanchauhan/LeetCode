f749c063e5409b137ccab85e74ee8ec56642be5b
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-03 23:26:58 +0530

    Flatten Binary Tree to Linked List done and added to Day-1

---
+++ Day-1/LeetCode_114.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode getTail(TreeNode root)
+    {
+        if(root==null)
+        {
+            return null;
+        }
+        while(root.right!=null)
+        {
+            root=root.right;
+        }
+        return root;
+    }
+    public void flatten(TreeNode root) {
+        if(root==null)
+        {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        if(root.left!=null)
+        {
+            TreeNode lchild=root.left;
+            TreeNode rchild=root.right;
+            TreeNode tail_lchild=getTail(lchild);
+            
+            root.right=lchild;
+            tail_lchild.right=rchild;
+            root.left=null;
+        }
+    }
+}
