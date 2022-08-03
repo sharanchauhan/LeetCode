202bcc048d49c20c14fa997937ef6ae999633c53
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-03 22:57:36 +0530

    Remove Nth node from the end of the linked list done and added to Day-1

---
+++ Day-1/LeetCode_19.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow=head,fast=head;
+        int gap=n-1;
+        while(gap!=0)
+        {
+            fast=fast.next;
+            gap--;
+        }
+        //This is a condition which tells us that the head of the linked list is to be removed
+        if(fast.next==null)
+        {
+            return head.next;
+        }
+        ListNode prev=null;
+        while(fast.next!=null)
+        {
+            fast=fast.next;
+            prev=slow;
+            slow=slow.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
