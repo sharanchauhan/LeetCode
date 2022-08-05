ea20040a2dd64d292108a08b2f97845d8abb3662
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-05 21:50:14 +0530

    Reverse nodes in k-group done and added to Day-2

--- Day-2/LeetCode_25.java
+++ Day-2/LeetCode_25.java
@@ -9,7 +9,52 @@
  * }
  */
 class Solution {
+    public int size(ListNode head)
+    {
+        int count=0;
+        while(head!=null)
+        {
+            head=head.next;
+            count++;
+        }
+        return count;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode prev=null,curr=head;
+        while(curr!=null)
+        {
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        head=prev;
+        return head;
+    }
+    
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+        ListNode dummy=new ListNode(-1);
+        ListNode p1=dummy;
+        int work=(size(head))/k;
+        ListNode prev=head,curr=head;
+        while(work!=0)
+        {
+            int tmp=k-1;
+            while(tmp!=0)
+            {
+                curr=curr.next;
+                tmp--;
+            }
+            ListNode nbr=curr.next;
+            curr.next=null;
+            reverse(prev);
+            p1.next=curr;
+            p1=prev;
+            curr=prev=nbr;
+            work--;
+        }
+        p1.next=curr;
+        return dummy.next;
     }
 }
