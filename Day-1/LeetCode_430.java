5da5e0d4ae7a2b1697438da92c8f81e4c471b642
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-05 20:39:50 +0530

    Moved the file into the folder

---
+++ Day-1/LeetCode_430.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if(head==null)
+        {
+            return null;
+        }
+        Node curr=head;
+        while(curr!=null)
+        {
+            if(curr.child!=null)
+            {
+                Node ch=curr.child;
+                Node temp=ch;
+                while(temp.next!=null)
+                {
+                    temp=temp.next;
+                }
+                temp.next=curr.next;
+                if(curr.next!=null)
+                {
+                    curr.next.prev=temp;
+                }
+                curr.next=ch;
+                ch.prev=curr;
+                curr.child=null;
+            }
+            curr=curr.next;
+        }
+        return head;
+    }
+}
