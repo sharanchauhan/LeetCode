9639790d22439d25d5aa026dfffc74b7b880c2f2
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-20 00:11:35 +0530

    Longest Subarray With Equal Number Of Zeroes And Ones done and added

---
+++ Day-5/LongestSubarrayWithEqualNumberOfZeroesAndOnes.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr) {
+        // write your code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int no_one=0,no_zero=0,len=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==1)
+            {
+                no_one++;
+            }
+            else
+            {
+                no_zero++;
+            }
+            int diff=no_one-no_zero;
+            if(map.containsKey(diff))
+            {
+                len=Math.max(len,i-map.get(diff));
+            }
+            else
+            {
+                map.put(diff,i);
+            }
+        }
+        return len;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+}
