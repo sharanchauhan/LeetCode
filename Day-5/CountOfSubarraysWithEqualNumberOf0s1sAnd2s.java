2ce126c0bf76e48910d31b5d86e9ec76b9217768
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-20 00:02:08 +0530

    Count Of Subarrays With Equal Number Of 0s 1s And 2s done and added

---
+++ Day-5/CountOfSubarraysWithEqualNumberOf0s1sAnd2s.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr) {
+        // write your code here
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put(0+"#"+0,1);
+        int no_one=0,no_zero=0,no_two=0,count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==1)
+            {
+                no_one++;
+            }
+            else if(arr[i]==0)
+            {
+                no_zero++;
+            }
+            else
+            {
+                no_two++;
+            }
+            String diff=(no_one-no_zero)+"#"+(no_two-no_one);
+            if(map.containsKey(diff))
+            {
+                count+=map.get(diff);
+                map.put(diff,map.get(diff)+1);
+            }
+            else
+            {
+                map.put(diff,1);
+            }
+        }
+        return count;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+}
