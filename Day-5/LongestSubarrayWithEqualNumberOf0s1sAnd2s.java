e9b1a3ad654b9a85c0278ebe5395311c67c7438b
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-20 00:14:41 +0530

    Longest Subarray With Equal Number Of 0s 1s And 2s done and added

---
+++ Day-5/LongestSubarrayWithEqualNumberOf0s1sAnd2s.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr) {
+        // write your code here
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put(0+"#"+0,-1);
+        int no_one=0,no_zero=0,no_two=0,len=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==1)
+            {
+                no_one++;
+            }
+            else if(arr[i]==0)
+            {
+                no_zero++;
+            }
+            else
+            {
+                no_two++;
+            }
+            String diff=(no_one-no_zero)+"#"+(no_two-no_one);
+            if(map.containsKey(diff))
+            {
+                len=Math.max(len,i-map.get(diff));
+            }
+            else
+            {
+                map.put(diff,i);
+            }
+        }
+        return len;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+}
