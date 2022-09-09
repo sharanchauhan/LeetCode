fe86baf3765bdca6c517d6aee880f378c9d988b0
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-09-09 22:42:42 +0530

    Binary Tree Inorder Traversal done and added to Day-8

---
+++ Day-8/LeetCode_94.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Done using Morris method
+// Time Complexity - O(n)
+// Space Complexity - O(1) [Using only one variable]
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        while(root!=null)
+        {
+            if(root.left==null) //root can only be visited once
+            {
+                ans.add(root.val);
+                root=root.right;
+            }
+            else //root ->first visit or second visit
+            {
+                TreeNode rootp1=root.left;
+                while(rootp1.right!=null && rootp1.right!=root)
+                {
+                    rootp1=rootp1.right;
+                }
+                if(rootp1.right==null) //first visit
+                {
+                    rootp1.right=root; //connect
+                    root=root.left;
+                }
+                else if(rootp1.right==root) //second visit
+                {
+                    ans.add(root.val);
+                    rootp1.right=null; //disconnect
+                    root=root.right;
+                }
+            }
+        }
+        return ans;
+    }
+}
